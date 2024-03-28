f935353e9a7f7abd32755b564c3dfe8419a3d045
Filmon Fitwi <filmon.kidane08@gmail.com>
2024-03-27 19:09:52 -0500

    Adding Student and Teacher model to git, and also add SMS_Diagram

---
+++ SchoolManagementSystem/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required int AdmissionId { get; set;}
+        public DateTime DateOfBirth { get; set;}
+        public int PhoneNumber { get; set;}
+        public string? about { get; set;}
+        public int GenderId { get; set; }
+        public int AddressId {  get; set; }
+        public int DepartmentId {  get; set; }
+    }
+}
---
+++ SchoolManagementSystem/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public  int MobileNumber { get; set; }
+        public DateTime JoinedDate { get; set; }
+        public string? Qualification {  get; set; }
+        public string? Experience {  get; set; }
+        public string? about { get; set; }
+        public int GenderId { get; set; }
+        public int AddressId { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
